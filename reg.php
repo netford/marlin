2ce8b52f9f1bea1e4f572e5d0e1c955211aedf79
netford <konstpon2012@gmail.com>
2020-09-14 19:58:29 +0300

    Урок № 1(повтор)

---
+++ reg.php
@@ -0,0 +1,77 @@
+<?php
+/**
+    Parametrs:
+        string - $email
+
+    Description: поиск пользователя по электронному адресу
+
+    Return value: array
+**/
+function get_user_by_email($email) {
+
+};
+
+/**
+    Parametrs:
+        string - $email
+        string - $pasword
+
+    Description: добавить пользователя в БД
+
+    Return value: int (user_id)
+**/
+function add_user($email, $password) {
+
+};
+
+/**
+    Parametrs:
+        string - $name (ключ)
+        string - $message (знчение, текст сообщения)
+
+    Description: добавить пользователя в БД
+
+    Return value: null
+**/
+function add_user($email, $password) {
+
+};
+
+/**
+    Parametrs:
+        string - $name (ключ)
+        string - $message (знчение, текст сообщения)
+
+    Description: подготовить флеш сообщение
+
+    Return value: null
+**/
+function set_flash_message($name, $message) {
+
+};
+
+/**
+    Parametrs:
+        string - $name (ключ)
+
+    Description: вывести флеш сообщение
+
+    Return value: null
+**/
+function display_flash_message($name) {
+
+};
+
+/**
+    Parametrs:
+        string - $puth
+
+    Description: перенаправить на другую страницу
+
+    Return value: null
+**/
+function redirect_to($puth) {
+
+};
+
+
